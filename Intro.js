7ab52e0fa9cd291e6dae8ac8b183ece2d12491b4
BloginThemes <76254505+BloginThemes@users.noreply.github.com>
2020-12-19 19:56:01 +0500

    Create Intro.js

---
+++ Intro.js
@@ -0,0 +1,42 @@
+<!--
+
+{
+  "name": "Themezest Official",
+  "short_name": "Themezest",
+  "start_url": "https://www.themezest.eu.org/",
+  "display": "standalone",
+  "background_color": "#fff",
+  "theme_color": "#1976d2",
+  "description": "Themezest Official Blog Where to Share Tutorials and Tricks Blogger, Premium Blogger Template, Blogger Design, Blogger Widget, Blogger Tutorial, Social media tutorials and Blogspot for beginners Blogger HTML, Javascript, CSS, Templates, AdSense",
+  "icons": [{
+    "src": "https://1.bp.blogspot.com/-HKBVUtKpdOw/X94Mo66K37I/AAAAAAAAAX4/xwTLGCtxAogAU9k2M_kh1JRwcqdPD9SjACLcBGAsYHQ/s48/Themezest.blogspot.com.png",
+    "sizes": "48x48",
+    "type": "image/png"
+  }, {
+    "src": "https://1.bp.blogspot.com/-HKBVUtKpdOw/X94Mo66K37I/AAAAAAAAAX4/xwTLGCtxAogAU9k2M_kh1JRwcqdPD9SjACLcBGAsYHQ/s72/Themezest.blogspot.com.png",
+    "sizes": "72x72",
+    "type": "image/png"
+  }, {
+    "src": "https://1.bp.blogspot.com/-HKBVUtKpdOw/X94Mo66K37I/AAAAAAAAAX4/xwTLGCtxAogAU9k2M_kh1JRwcqdPD9SjACLcBGAsYHQ/s96/Themezest.blogspot.com.png",
+    "sizes": "96x96",
+    "type": "image/png"
+  }, {
+    "src": "https://1.bp.blogspot.com/-HKBVUtKpdOw/X94Mo66K37I/AAAAAAAAAX4/xwTLGCtxAogAU9k2M_kh1JRwcqdPD9SjACLcBGAsYHQ/s144/Themezest.blogspot.com.png",
+    "sizes": "144x144",
+    "type": "image/png"
+  }, {
+    "src": "https://1.bp.blogspot.com/-HKBVUtKpdOw/X94Mo66K37I/AAAAAAAAAX4/xwTLGCtxAogAU9k2M_kh1JRwcqdPD9SjACLcBGAsYHQ/s168/Themezest.blogspot.com.png",
+    "sizes": "168x168",
+    "type": "image/png"
+  }, {
+    "src": "https://1.bp.blogspot.com/-HKBVUtKpdOw/X94Mo66K37I/AAAAAAAAAX4/xwTLGCtxAogAU9k2M_kh1JRwcqdPD9SjACLcBGAsYHQ/s192/Themezest.blogspot.com.png",
+    "sizes": "192x192",
+    "type": "image/png"
+  }, {
+    "src": "https://1.bp.blogspot.com/-HKBVUtKpdOw/X94Mo66K37I/AAAAAAAAAX4/xwTLGCtxAogAU9k2M_kh1JRwcqdPD9SjACLcBGAsYHQ/s1600/Themezest.blogspot.com.png",
+    "sizes": "512x512",
+    "type": "image/png"
+  }]
+}
+
+-->
